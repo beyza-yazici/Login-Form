891cbb7140179595bc6e1306f623d8b121ad6b68
beyza-yazici <beyzanyazici@hotmail.com>
2024-12-27 14:41:18 +0300

    login done

---
+++ cypress/support/e2e.js
@@ -0,0 +1,44 @@
+// ***********************************************************
+// This example support/e2e.js is processed and
+// loaded automatically before your test files.
+//
+// This is a great place to put global configuration and
+// behavior that modifies Cypress.
+//
+// You can change the location of this file or turn off
+// automatically serving support files with the
+// 'supportFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/configuration
+// ***********************************************************
+
+// Import commands.js using ES2015 syntax:
+import './commands';
+
+describe("A template spec", () => {
+    it("passes", () => {
+      cy.visit("http://localhost:5173");
+      cy.get('[data-cy="email"]').type("erdem.guntay@wit.com.tr");
+      cy.get('[data-cy="password"]').type("9fxIH0GXesEwH_I");
+      // 2nd way--> cy.get('[data-cy="terms').click();
+      cy.contains("I agree").click();
+      cy.get('[data-cy="submit"]').click();
+    });
+  
+    it("shows one error for invalid email.", () => {
+      cy.visit("http://localhost:5173");
+      cy.get('[data-cy="email"]').type("erdem.gun");
+      cy.get('[data-cy="error"]').should("have.length", 1);
+      cy.get('[data-cy="error"]').contains(/Please enter/);
+    });
+  
+    it("shows one error for invalid email.", () => {
+      cy.visit("http://localhost:5173");
+      cy.get('[data-cy="email"]').type("erdem.guntay@wit.com.tr");
+      cy.get('[data-cy="password"]').type("9fx");
+      cy.get('[data-cy="error"]').should("have.length", 1);
+      cy.get('[data-cy="error"]').contains(/Please enter/);
+      cy.get('[data-cy="error"]').contains(/Password must/);
+    });
+})
