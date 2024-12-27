d2dceefff024621a9652ade1ca7a7731635643b3
beyza-yazici <beyzanyazici@hotmail.com>
2024-12-27 14:49:09 +0300

    success done

---
+++ src/components/Success.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Alert } from 'reactstrap';
+
+const Success = () => {
+    return (
+        <>
+            <Alert>
+                <h1>Login Successful!</h1>
+            </Alert>
+        </>
+    );
+};
+
+export default Success;
